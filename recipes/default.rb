cd30d90e0e2f5189cbb6813f6c04fe4012092e18
Sam Brown <samuel.t.brown@gmail.com>
2013-10-22 16:09:22 -0400

    Removed nginx installation/config

--- recipes/default.rb
+++ recipes/default.rb
@@ -21,7 +21,6 @@
 # If this is important to you - BACK IT UP, or use a SQL DB thats already backed up for you.
 #
 
-package "nginx"
 package "git"
 package "sqlite"
 package "sqlite-devel"
@@ -63,10 +62,6 @@ end
   end
 end
 
-file "/etc/nginx/sites-enabled/default" do
-  action :delete
-end
-
 template "#{node.sensu.admin.base_path}/sensu-admin-unicorn.rb" do
   user node.sensu.admin.user
   group node.sensu.admin.user
@@ -76,56 +71,12 @@ template "#{node.sensu.admin.base_path}/sensu-admin-unicorn.rb" do
             :backend_port => node.sensu.admin.backend_port)
 end
 
-service "nginx" do
-  supports :status => true, :restart => true, :reload => true
-end
-
-template "#{node.sensu.admin.base_path}/sensu-admin-nginx.conf" do
-  user node.sensu.admin.user
-  group node.sensu.admin.user
-  source "sensu-admin-nginx.conf.erb"
-  variables(:host => node.sensu.admin.host,
-            :base_path => node.sensu.admin.base_path,
-            :http_port => node.sensu.admin.http_port,
-            :https_port => node.sensu.admin.https_port,
-            :backend_port => node.sensu.admin.backend_port)
-  notifies :restart, resources(:service => "nginx"), :delayed
-end
-
 template "/etc/init.d/sensu-admin" do
   source "unicorn.init.erb"
   owner "root"
   group "root"
   mode "0755"
   variables(:base_path => node.sensu.admin.base_path)
-end
-
-directory "/etc/nginx/sites-available" do
-  action :create
-end
-
-directory "/etc/nginx/sites-enabled" do
-  action :create
-end
-
-link "/etc/nginx/sites-available/sensu-admin.conf" do
-  to "#{node.sensu.admin.base_path}/sensu-admin-nginx.conf"
-end
-
-link "/etc/nginx/sites-enabled/sensu-admin.conf" do
-  to "/etc/nginx/sites-available/sensu-admin.conf"
-end
-
-ssl = data_bag_item("sensu", "ssl")
-
-file "#{node.sensu.admin.base_path}/server-cert.pem" do
-  content ssl["client"]["cert"]
-  mode 0644
-end
-
-file "#{node.sensu.admin.base_path}/server-key.pem" do
-  content ssl["client"]["key"]
-  mode 0600
 end
 
 deploy_revision "sensu-admin" do
